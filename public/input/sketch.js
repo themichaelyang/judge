d350022f48fcc7021bd5293d15898464a8ea7a27
Zoe Wells <zoewells@10-20-99-27.dynapool.wireless.nyu.edu>
2019-09-28 17:09:53 -0400

    add gitignore

--- public/input/sketch.js
+++ public/input/sketch.js
@@ -1,5 +1,12 @@
 let players = {};
 let adjectives;
+let currentWord;
+let halfWidth;
+let rollover;
+let textX, textY, textW, textH;
+let offsetX, offsetY;
+let dragging;
+let i;
 
 const ADD_PLAYER = 'add-player';
 
@@ -8,8 +15,23 @@ function setup() {
 
   // Listen for confirmation of connection
   socket.on('connect', () => initSocket(socket));
+
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  textSize(16);
+  noStroke();
+  textAlign(CENTER);
+  halfWidth = windowWidth/2;
+  halfheight = halfWidth*0.7;
+  textX = halfWidth-60;
+  textY = halfheight+50;
+  textW = 120;
+  textH = 30;
+  offsetX = 0;
+  offsetY = 0;
+  dragging = false;
+  i = 0;
+  background(20);
+  currentword = "";
 }
 
 function initSocket(socket) {
@@ -22,4 +44,67 @@ function initSocket(socket) {
   socket.on('adjectives', (data) => {
     adjectives = data.adjectives[socket.id];
   });
+}
+
+function draw(){
+  if(adjectives){
+    currentword = adjectives[i];
+    background(20);
+
+    fill(255, 204, 0);
+    rect(10, 10, halfWidth-10, halfheight);
+    fill(105, 186, 73);
+    rect(halfWidth+10, 10, halfWidth-20, halfheight);
+
+    fill(200, 200, 200);
+
+    if (dragging) {
+      textX = mouseX + offsetX;
+      textY = mouseY + offsetY;
+    }else{
+      textX = halfWidth-50;
+      textY = halfheight+50;
+    }
+
+    rect(textX, textY, textW, textH);
+
+    fill(20,20,20);
+    text(currentword, textX+60, textY+20);
+
+    if (mouseX > textX && mouseX < textX + textW && mouseY > textY && mouseY < textY + textH) {
+      rollover = true;
+    }
+    else {
+      rollover = false;
+    }
+
+}
+
+function mousePressed(){
+  if (rollover) {
+    dragging = true;
+  }
+  if (dragging){
+    offsetX = textX-mouseX;
+    offsetY = textY-mouseY;
+  }
+}
+
+}
+
+function mouseReleased() {
+  //if it was dragging
+  if (dragging){
+    //if over yellow
+    if (mouseX > 10 && mouseX < 10 + (halfWidth-10) && mouseY > 10 && mouseY < 10 + halfheight){
+    console.log("Yellow is " + currentword);
+    i ++;
+
+    //if over green
+    }else if(mouseX > halfWidth+10 && mouseX < halfWidth+10 + halfWidth-20 && mouseY > 10 && mouseY < 10 + halfheight){
+    console.log("Green is " + currentword);
+    i ++;
+    }
+    dragging = false;
+  }
 }
